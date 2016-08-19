2e40814a8d0fa0c8fb2c28cfef84eeae4ee1a0c0
Kym Derrick Uy <kymupd126@gmail.com>
2016-08-19 17:12:35 -0400

    finished design of reminders list, ordered and filtered

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,7 +7,12 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
 
     $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
         $scope.meds = meds.data;
+        if($scope.meds.length == 0){
+          angular.element('#no_task' ).css('display', 'inline');
+        }
     });
+
+    $scope.currentTime = moment().format('h:mm:ss a');
 
     $window.setInterval(function () {
         $scope.currentTime = moment().format('h:mm:ss a');
@@ -15,29 +20,55 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
     }, 1000);
 
     $scope.ctr = 0;
-    $scope.currentDate = moment().format('MMMM Do YYYY');
+    $scope.currentDate = "Today";
 
     $scope.increment = function(){
+      angular.element('#no_task' ).css('display', 'none');
       $scope.ctr++;
-      $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      if($scope.ctr == 0){
+        $scope.currentDate = "Today";
+      }else{
+        $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      }
       var start = moment().add($scope.ctr, 'day').format('MM/DD/YYYY'),
           end = moment().add($scope.ctr + 1, 'day').format('MM/DD/YYYY');
 
       $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
           $scope.meds = meds.data;
+          if($scope.meds.length == 0){
+            angular.element('#no_task' ).css('display', 'inline');
+          }
       });
     }
 
     $scope.decrement = function(){
+      angular.element('#no_task' ).css('display', 'none');
       $scope.ctr--;
-      $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      if($scope.ctr == 0){
+        $scope.currentDate = "Today";
+      }else{
+        $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      }
       var start = moment().add($scope.ctr, 'day').format('MM/DD/YYYY'),
           end = moment().add($scope.ctr + 1, 'day').format('MM/DD/YYYY');
 
       $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+        console.log($scope.meds);
           $scope.meds = meds.data;
+          if($scope.meds.length == 0){
+            console.log("LOL");
+            angular.element('#no_task' ).css('display', 'inline');
+          }
       });
     }
 
 
 });
+
+angular.module('medicationReminderApp').filter('cmdate', [
+    '$filter', function($filter) {
+        return function(input, format) {
+            return $filter('date')(new Date(input), format);
+        };
+    }
+]);
