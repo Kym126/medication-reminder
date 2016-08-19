4e0cb8bc8896604c0b9fa408455d569bc03269f3
Kym Derrick Uy <kymupd126@gmail.com>
2016-08-18 21:57:13 -0400

    made an increment and decrement date button

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -10,8 +10,34 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
     });
 
     $window.setInterval(function () {
-        $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        $scope.currentTime = moment().format('h:mm:ss a');
         $scope.$apply();
     }, 1000);
+
+    $scope.ctr = 0;
+    $scope.currentDate = moment().format('MMMM Do YYYY');
+
+    $scope.increment = function(){
+      $scope.ctr++;
+      $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      var start = moment().add($scope.ctr, 'day').format('MM/DD/YYYY'),
+          end = moment().add($scope.ctr + 1, 'day').format('MM/DD/YYYY');
+
+      $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+          $scope.meds = meds.data;
+      });
+    }
+
+    $scope.decrement = function(){
+      $scope.ctr--;
+      $scope.currentDate = moment().add($scope.ctr, 'day').format('MMMM Do YYYY');
+      var start = moment().add($scope.ctr, 'day').format('MM/DD/YYYY'),
+          end = moment().add($scope.ctr + 1, 'day').format('MM/DD/YYYY');
+
+      $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+          $scope.meds = meds.data;
+      });
+    }
+
 
 });
